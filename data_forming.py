2244df0337fd3d62875dc0e9ed84fb45c1fcae5c
shuhei <kawashu0723@gmail.com>
2017-04-08 13:43:41 +0900

    categoryを番号に変換

--- data_forming.py
+++ data_forming.py
@@ -11,7 +11,11 @@ def find_all_files(directory):
             yield os.path.join(root, f)
 
 
-data = pd.DataFrame({'title': [], 'category': []})
+data = pd.DataFrame({
+    'title': [],
+    'category': []
+})
+
 for file in find_all_files('./raw_data'):
     path, ext = os.path.splitext(file)
     if ext != '.csv':
@@ -20,5 +24,16 @@ for file in find_all_files('./raw_data'):
     with open(file, "r") as f:
         csv_data = pd.read_csv(file)
         data = pd.concat([data, csv_data[['title', 'category']]])
+
+data['category'] = data['category'].replace({
+    '政治': '1',
+    '社会': '2',
+    '国際': '3',
+    'ビジネス': '4',
+    '科学・文化': '5',
+    '暮らし': '6',
+    '気象・災害': '7',
+    'スポーツ': '8'
+})
 
 data.to_csv('sample.csv', index=False)
