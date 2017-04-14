16e983f944cb9bdf9c7cdb1de23d1ac39cc51420
shuhei <kawashu0723@gmail.com>
2017-04-14 21:10:29 +0900

    メソッドにした

--- input_data.py
+++ input_data.py
@@ -9,41 +9,42 @@ mecab = MeCab.Tagger('-d /usr/lib/mecab/dic/mecab-ipadic-neologd -Owakati')
 word_dictionary = {}
 word_arrays = []
 input_data = []
-
-with open("word_dictionary.txt", "r") as file:
-    for line in file:
-        line = line.replace('\n', '')
-        l = line.split(': ')
-        word_dictionary[l[1]] = l[0]
-
-for line in learning_data['title']:
-    text = mecab.parseToNode(line)
-    word_array = []
-    while text:
-        if text.surface == '':
-            text = text.next
-            continue
-        word_array.append(word_dictionary[text.surface])
-        text = text.next
-    word_arrays.append(word_array)
-
-max_count = 0
-for word_array in word_arrays:
-    if max_count < len(word_array):
-        max_count = len(word_array)
-
-for word_array in word_arrays:
-    less_count = max_count - len(word_array)
-    for _ in range(less_count):
-        word_array.append(0)
-    input_data.append(word_array)
-
 correct_data = []
-for line in learning_data['category']:
-    correct_data.append(line)
 
-validation_size = 4000
-validation_text = input_data[:validation_size]
-validation_labels = correct_data[:validation_size]
-train_text = input_data[validation_size:]
-train_labels = correct_data[validation_size:]
+
+def read_data_sets(validation_size=4000):
+    with open("word_dictionary.txt", "r") as file:
+        for line in file:
+            line = line.replace('\n', '')
+            l = line.split(': ')
+            word_dictionary[l[1]] = l[0]
+
+    for line in learning_data['title']:
+        text = mecab.parseToNode(line)
+        word_array = []
+        while text:
+            if text.surface == '':
+                text = text.next
+                continue
+            word_array.append(word_dictionary[text.surface])
+            text = text.next
+        word_arrays.append(word_array)
+
+    max_count = 0
+    for word_array in word_arrays:
+        if max_count < len(word_array):
+            max_count = len(word_array)
+
+    for word_array in word_arrays:
+        less_count = max_count - len(word_array)
+        for _ in range(less_count):
+            word_array.append(0)
+        input_data.append(word_array)
+
+    for line in learning_data['category']:
+        correct_data.append(line)
+
+    validation_text = input_data[:validation_size]
+    validation_labels = correct_data[:validation_size]
+    train_text = input_data[validation_size:]
+    train_labels = correct_data[validation_size:]
---
+++ news_cnn.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+import input_data
+
+
+def main():
+    input_data.read_data_sets()
+
+if __name__ == '__main__':
+    main()
